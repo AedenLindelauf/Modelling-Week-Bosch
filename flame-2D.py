147030c3a5f0c08931e4d38c007d02211ea4bcf9
Vertic 03 <stanys.domantas@gmail.com>
2025-11-05 12:18:36 +0100

    flame-2D simulation finished (doesn't work)

--- flame-2D.py
+++ flame-2D.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 import pandas as pd
 import scipy.optimize as sp
 
@@ -7,8 +8,8 @@ import scipy.optimize as sp
 A, B, C, D = 0.6079, -2.554, 7.31, 1.23
 
 # --- Domain discretization ---
-x0, xN, dx = 0.0, 5.0, 0.1
-y0, yN, dy = 0.0, 5.0, 0.1
+x0, xN, dx = 0.0, 3.0, 0.1
+y0, yN, dy = 0.0, 3.0, 0.1
 t_final, dt = 15.0, 0.1
 radius = 1.5
 
@@ -20,12 +21,13 @@ Nt = round(t_final / dt + 1)
 N_radius = round(radius / dx)
 
 # --- Velocity field ---
-u = np.zeros((Nx, Ny, Nt, 2))
+u_x = np.zeros((Nx, Ny, Nt))
+u_y = np.ones((Nx, Ny, Nt)) * 0.14
 #u[0] = 1.0  # only at boundary
 
 # --- Initial conditions ---
-u_constant = 2.0
-s_L_constant = 1.0
+u_constant = 0.14
+s_L_constant = 0.0725887
 
 
 G = np.zeros((Nx, Ny, Nt))
@@ -37,15 +39,16 @@ for i in range(Nx):
 # x-axis boundary condition (constant over time)
 for i in range(Nx):
     for j in range(Nt):
-        G[i][0][j] = (u_constant**2 - s_L_constant**2)**0.5 / s_L_constant * (i*Nx - radius)
+        G[i][0][j] = (u_constant**2 - s_L_constant**2)**0.5 / s_L_constant * (i*dx - radius) + 0
 
 # y-axis boundary condition (constant over time)
 for i in range(Ny):
     for j in range(Nt):
-        G[0][i][j] = (u_constant**2 - s_L_constant**2)**0.5 / s_L_constant * (i*Ny - radius) + i*Ny
+        G[0][i][j] = (u_constant**2 - s_L_constant**2)**0.5 / s_L_constant * (0 - radius) + i*dy
 
 
-phi = np.ones((Nx, Ny, Nt)) * 1.0
+phi = np.ones((Nx, Ny, Nt)) * 0.5
+phi_constant = 0.5
 
 # --- Time stepping ---
 for t in range(1, Nt):
@@ -53,48 +56,28 @@ for t in range(1, Nt):
         for x in range(1, Nx):
 
             # phi is constant - NO WORK NEEDED! YAY!
+            # Later will implement varying phi
+
+            # Local laminal flame speed
             s_L = A * (phi[x,y,t] ** B) * np.exp(-C * (phi[x,y,t] - D) ** 2)
+
+            # Local value of G, from the G equation
+            # Resulting equation (for G[x,y,t]) cannot be solved explicitly, so use fsolve instead
             def diff_eq(z):
-                return (z - G[x,y,t-1])/dt + u[]
-
-
-
-for t in range(1, Nt):
-    for y in range(1, Nx):
-        #G_old = G.copy()
-        #phi_old = phi.copy()
-
-        phi[y, t] = (dx * phi[y, t-1] + dt * u[y-1] * phi[y-1, t]) / (dx + dt * u[y-1])
-        s_L = A * (phi[y,t] ** B) * np.exp(-C * (phi[y,t] - D) ** 2)
-        def diff_eq(z):
-            return (z - G[y, t-1])/dt + u[y-1] * (z - G[y-1, t])/dx - s_L * abs(z - G[y-1, t])/dx
-        G[y, t] = sp.fsolve(diff_eq, x0 = G[y-1, t])[0]
+                return ((z - G[x,y,t-1])/dt 
+                        + u_x[x-1, y, t] * (z - G[x-1,y,t])/dx 
+                        + u_y[x, y-1, t] * (z - G[x,y-1,t])/dy 
+                        - s_L * (((z - G[x-1,y,t])/dx) ** 2 + ((z - G[x,y-1,t])/dy) ** 2) ** 0.5)
+            G[x,y,t] = sp.fsolve(diff_eq, x0 = G[x-1,y,t])[0]
 
 
 print(G)
 print(phi)
 print(s_L)
-"""for n in range(Nt):
-    G_old = G.copy()
-    phi_old = phi.copy()
-    
-    # update phi using forward Euler + upwind
-    for i in range(1, Nx):
-        phix = (phi_old[i] - phi_old[i-1]) / dx
-        phi[i] = phi_old[i] - dt * u[i] * phix
-    phi[0] = 0.0  # boundary condition
 
-    # compute s_L(phi)
-    
-    
-    # update G using forward Euler
-    for i in range(1, Nx):
-        Gx = (G_old[i] - G_old[i-1]) / dx
-        G[i] = G_old[i] + dt * (-u[i] * Gx + s_L[i] * abs(Gx))
-    G[0] = -1.0  # boundary condition"""
 
 # --- Results ---
-df = pd.DataFrame({"x": x, "G(x,15)": [G[i, Nt - 1] for i in range(Nx)], "phi(x,15)": [phi[i, Nt - 1] for i in range(Nx)]})
+"""df = pd.DataFrame({"x": x, "G(x,15)": [G[i, Nt - 1] for i in range(Nx)], "phi(x,15)": [phi[i, Nt - 1] for i in range(Nx)]})
 print(df.head(), "\n...\n", df.tail())
 
 # --- Plot results ---
@@ -110,4 +93,11 @@ plt.title(f"phi(x, t={t_final})")
 plt.xlabel("x"); plt.ylabel("phi")
 
 plt.tight_layout()
+plt.show()"""
+final_heatmap_data = [[G[i,j,Nt-1] for i in range(Nx)] for j in range(Ny)]
+final_heatmap_data = np.array(final_heatmap_data)
+plt.imshow(final_heatmap_data, extent = [x0, xN-x0, y0, yN-y0], origin = 'lower')
+plt.colorbar(label = "G value")
+plt.title("Heatmap at final time step")
+
 plt.show()
